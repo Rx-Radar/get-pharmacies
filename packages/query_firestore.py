0ab3052b0f53e4759484e3339b45b5b2195e6e98
Owen Dolan <odolan27@gmail.com>
2024-05-06 23:02:30 -0400

    test

--- packages/query_firestore.py
+++ packages/query_firestore.py
@@ -64,7 +64,7 @@ def db_geo_query(db, search_geohashes, precision):
     for geohash in search_geohashes:
         try:
             # Make query to 'users' collection where 'geohash' field is equal to the specified value
-            query_ref = db.collection('pharmacies').where('brand', '==', 'CVS')
+            query_ref = db.collection('pharmacies').where('location.lat', '==', 42.3620776)
             # query_ref = db.collection('pharmacies').where(f'location.geohash_s{str(precision)}', '==', geohash)
 
             # Execute the query and get the resulting documents
