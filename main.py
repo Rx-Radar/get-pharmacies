092761fdc0fcbd8554ae226ed0b82294be0d8617
Owen Dolan <odolan27@gmail.com>
2024-05-17 12:16:43 -0400

    update get-pharmacies move push to db in query places

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import jsonify, request
 from packages import query_places
 from packages import query_firestore
 import json
-import uuid
 import yaml
 import os
 
@@ -19,8 +18,7 @@ config = load_yaml_file('config.yaml')
 
 env = os.getenv("deployment_env")
 
-GGL_PLACES_API_KEY = config[env]["places"]["api_key"] 
-FIREBASE_PHARMACIES_DB = config[env]["firebase"]["pharmacy_db"] 
+## ----- no current imports from env ------ ##
 
 # Initialize Firebase Admin SDK with the service account key
 cred = credentials.Certificate("firebase_creds.json")  # Update with your service account key file  
@@ -68,39 +66,13 @@ def main(request):
 
     # query new ones from google places API if we have less than the target amount
     ggl_formated_location = f'{user_lat}, {user_lon}'
-    new_pharmacies = query_places.find_new_nearby_pharmacies(GGL_PLACES_API_KEY, ggl_formated_location)
-
-    # add new pharmacies to db
-    add_pharmacies_to_db(db=db, new_pharmacies=new_pharmacies)
+    new_pharmacies = query_places.find_new_nearby_pharmacies(db=db, location=ggl_formated_location)
 
     # if after places search, we found more pharmacies from db query
     if len(pharmacies) > len(new_pharmacies):
         return pharmacies
 
     return new_pharmacies[:num_pharmacies] # return the first num_pharmacies found from places
-
-
-# adds new pharmacies to the database
-def add_pharmacies_to_db(db, new_pharmacies):
-    for pharmacy in new_pharmacies:
-        try:
-            pharmacies_ref = db.collection(FIREBASE_PHARMACIES_DB)
-            query = pharmacies_ref.where('ggl_place_id', '==', pharmacy['ggl_place_id']).limit(1)
-            
-            docs = list(query.stream())
-            
-            # Check if the query returns any document
-            if len(docs) > 0:
-                # pharmacy exists in db
-                continue
-            else:
-                # pharmacy does not exist in db — add new pharmacy
-                new_doc_ref = pharmacies_ref.document(pharmacy["pharm_uuid"])
-                new_doc_ref.set(pharmacy)
-            
-        except Exception as e:
-            continue # if checking for a pharmacy/ adding it to db fails, continue to the next pharmacy
-
         
 
 # validate request body data
